01778c7560b237b285c47a2d0e8d48a27a82bf20
Carlos Mariano Belmonte <cbelmonte.laboral@gmail.com>
2022-05-14 12:10:07 -0300

    Se agrega Grid al NavBar para mejorar el posicionamiento

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -2,6 +2,7 @@
 import './NavBar.css';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
+import Grid from '@mui/material/Grid';
 import Button from '@mui/material/Button';
 import Avatar from '@mui/material/Avatar';
 import ShoppingCartTwoToneIcon from '@mui/icons-material/ShoppingCartTwoTone';
@@ -10,42 +11,59 @@ const NavBar = () => {
     return (
         <AppBar position="static">
             <Toolbar>
-                <div className="container-logo">
-                    <Avatar
-                        className='nav-logo-img'
-                        alt="Logo ENGENIUM"
-                        src="./logo.png"
-                        sx={{ width: 60, height: 60 }}
-                    />
-                    <h1>ENGENIUM<span>|</span></h1>                      
-                </div>
-                <div className="container-list"> 
-                    <ul>
-                        <li>
-                            <Button color="inherit" className="navbar__btn">Inicio</Button>
-                        </li>
-                        <li>
-                            <Button color="inherit" className="navbar__btn">Quienes somos</Button>
-                        </li>
-                        <li>
-                            <Button color="inherit" className="navbar__btn">Proyectos</Button>
-                        </li>
-                        <li>
-                            <Button color="inherit" className="navbar__btn">Productos</Button>
-                        </li>
-                        <li>
-                            <Button color="inherit" className="navbar__btn">Servicios</Button>
-                        </li>
-                        <li>
-                            <Button color="inherit" className="navbar__btn">Contacto</Button>
-                        </li>
-                        <li>
-                            <Button color="inherit" className='carrito__btn'>
-                                <ShoppingCartTwoToneIcon /> 
-                            </Button>
-                        </li>
-                    </ul>                
-                </div>             
+            <Grid container spacing={0}>
+                <Grid item md={3}>
+                    <div className="container-logo">
+                        <Avatar
+                            className='nav-logo-img'
+                            alt="Logo ENGENIUM"
+                            src="./logo.png"
+                            sx={{ width: 60, height: 60 }}
+                        />
+                        <h1>ENGENIUM<span>|</span></h1>                      
+                    </div>    
+                </Grid>
+                <Grid item md={7} >
+                    <div className="container-list"> 
+                        <ul>
+                            <li>
+                                <Button color="inherit" className="navbar__btn">Inicio</Button>
+                            </li>
+                            <li>
+                                <Button color="inherit" className="navbar__btn">Quienes somos</Button>
+                            </li>
+                            <li>
+                                <Button color="inherit" className="navbar__btn">Proyectos</Button>
+                            </li>
+                            <li>
+                                <Button color="inherit" className="navbar__btn">Productos</Button>
+                            </li>
+                            <li>
+                                <Button color="inherit" className="navbar__btn">Servicios</Button>
+                            </li>
+                            <li>
+                                <Button color="inherit" className="navbar__btn">Contacto</Button>
+                            </li>
+                        </ul>                
+                    </div>
+                </Grid>
+                <Grid 
+                    item md={2}
+                    container
+                    direction="row"
+                    justifyContent="flex-end"
+                    alignItems="center"
+                >
+                    <div className="container-cart">
+                        <Button color="inherit" className='carrito__btn'>
+                            <ShoppingCartTwoToneIcon /> 
+                        </Button>  
+                    </div>               
+                                            
+                </Grid>
+            </Grid>
+                
+                                 
             </Toolbar>
         </AppBar>
     ) 
