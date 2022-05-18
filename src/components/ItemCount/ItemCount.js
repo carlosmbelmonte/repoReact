8a20d2fd7e68b3fedc2f205234c01a0a07e77d6b
Carlos Mariano Belmonte <cbelmonte.laboral@gmail.com>
2022-05-18 13:03:23 -0300

    Se agrega en boton de AGREGA AL CARRITO

--- src/components/ItemCount/ItemCount.js
+++ src/components/ItemCount/ItemCount.js
@@ -5,13 +5,17 @@ import React, { useState } from 'react';
 const ItemCount = ({stock, initial}) => {
     const [count, setCount] = useState(initial);
     return (
-        <div className="itemcount-container">
-            <Button variant="outlined" onClick={() => setCount(count - 1)} disabled={count<1}>-</Button>
-            <p>{count}</p>
-            <Button variant="outlined" onClick={() => setCount(count + 1)} disabled={count>=stock}>+</Button>      
+        <div>
+            <div className="itemcount-container">
+                <Button variant="outlined" onClick={() => setCount(count - 1)} disabled={count<1}>-</Button>
+                <p>{count}</p>
+                <Button variant="outlined" onClick={() => setCount(count + 1)} disabled={count>=stock}>+</Button>          
+            </div>
+            <div>
+                <button variant="contained" class="item-btn__carrito">AGREGAR AL CARRITO</button>
+            </div>
         </div>      
-    )
-    
+    )    
 }
 
 export default ItemCount
