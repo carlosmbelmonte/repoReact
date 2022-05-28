d5c6751e767e29698767b3ca617e81c1e9ac8e7a
Carlos Mariano Belmonte <cbelmonte.laboral@gmail.com>
2022-05-28 17:56:54 -0300

    Queda pendiente para agregar categorias de productos

---
+++ src/pages/categorias.js
@@ -0,0 +1,14 @@
+/*import ItemListContainer from "../components/ItemListContainer/ItemListContainer";
+import automatizacion from "../utils/productosMock";
+import medidores from "../utils/medidoresMock";*/
+
+
+const Categoria = () => {
+    return(
+        <>
+            <h1>PENDIENTE DE CONSTRUCCION</h1>
+        </>        
+    )
+}
+
+export default Categoria
