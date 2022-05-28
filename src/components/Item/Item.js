954c0a0d6e95abb117f6114a10d973600c278e7b
Carlos Mariano Belmonte <cbelmonte.laboral@gmail.com>
2022-05-27 22:25:26 -0300

    Al boton de Detalles se le agrega la navegabilidad al producto especifico

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -3,8 +3,9 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import ItemCount from '../ItemCount/ItemCount';
+import { Link } from 'react-router-dom';
 
-const Item = ({nombreProducto, precioProducto, imagenProducto, stockProducto}) =>{
+const Item = ({nombreProducto, precioProducto, imagenProducto, stockProducto, idProducto}) =>{
     return(
         <Card sx={{ minWidth: 275 }} className="item-card">
             <CardContent>
@@ -14,7 +15,7 @@ const Item = ({nombreProducto, precioProducto, imagenProducto, stockProducto}) =
                     </div>
                     <p>{nombreProducto}</p>
                     <span>u$s {precioProducto}</span>
-                    <Button variant="contained" className="card__btn">Detalle</Button>
+                    <Link to ={`/product/${idProducto}`} className="link-color"><Button variant="contained" className="card__btn">Detalle</Button></Link>
                     <ItemCount stock={stockProducto} initial={1} onAdd={nombreProducto}/>
                 </div>
             </CardContent>
