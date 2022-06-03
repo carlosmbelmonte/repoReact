b52a28a858f364480cad61c9be2bd85409a70926
Carlos Mariano Belmonte <cbelmonte.laboral@gmail.com>
2022-06-03 11:51:01 -0300

    Se agrega CartProvider al return

--- src/App.js
+++ src/App.js
@@ -7,31 +7,35 @@ import Products from './pages/products';
 import Home from './pages/home';
 import Detalle from './pages/detalleProducto';
 import Categoria from './pages/categorias';
+import {CartProvider} from './context/CartContext';
 
 function App() { 
   return (
     //JSX   
       <div>
-        <BrowserRouter>
-          <NavBar>
-            <CartWidget/>
-          </NavBar>
+        <CartProvider>
+          <BrowserRouter>
+            <NavBar>
+              <CartWidget/>
+            </NavBar>
 
-          <Container>
-            <Routes>
-                <Route exact path='/' element={<Products/>}/> 
-                <Route exact path='/home' element={<Home/>}/>
-                <Route exact path='/aboutus' element={<h1>QUIENES SOMOS</h1>}/>
-                <Route exact path='/projects' element={<h1>PROYECTOS</h1>}/>
-                <Route exact path='/products' element={<Products/>}/>
-                <Route exact path='products/:category' element={<Categoria/>}/>
-                <Route path='/product/:id' element={<Detalle/>}/>
-                <Route exact path='/services' element={<h1>SERVICIOS</h1>}/>  
-                <Route exact path='/contacts' element={<h1>CONTACTO</h1>}/>     
-                <Route path='*' element={<h1>404 - NO EXISTE LA PAGINA</h1>}/>          
-            </Routes>
-          </Container>
-        </BrowserRouter>
+            <Container>
+              <Routes>
+                  <Route exact path='/' element={<Products/>}/> 
+                  <Route exact path='/home' element={<Home/>}/>
+                  <Route exact path='/aboutus' element={<h1>QUIENES SOMOS</h1>}/>
+                  <Route exact path='/projects' element={<h1>PROYECTOS</h1>}/>
+                  <Route exact path='/products' element={<Products/>}/>
+                  <Route exact path='products/:category' element={<Categoria/>}/>
+                  <Route path='/product/:id' element={<Detalle/>}/>
+                  <Route exact path='/services' element={<h1>SERVICIOS</h1>}/>  
+                  <Route exact path='/contacts' element={<h1>CONTACTO</h1>}/>     
+                  <Route path='*' element={<h1>404 - NO EXISTE LA PAGINA</h1>}/>          
+              </Routes>
+            </Container>
+          </BrowserRouter>  
+        </CartProvider>
+        
         {/*
         <NavBar>
           <CartWidget/>
