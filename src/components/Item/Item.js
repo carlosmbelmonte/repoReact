3622ff8f6d1650e4f8948ddfb86e56ffaeb5cb11
Carlos Mariano Belmonte <cbelmonte.laboral@gmail.com>
2022-06-03 21:26:48 -0300

    Se modifica Item.js se quita el componente ItemCount y solo se lo deja en el sector de detalle

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -2,13 +2,16 @@ import './Item.css';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
-import ItemCount from '../ItemCount/ItemCount';
+/*import ItemCount from '../ItemCount/ItemCount';*/
 import { Link } from 'react-router-dom';
 /*import { useState } from 'react';*/
+/*import { useContext } from 'react';
+import CartContext from '../../context/CartContext';*/
 
 let variableH;
 const Item = ({nombreProducto, precioProducto, imagenProducto, stockProducto, idProducto,title}) =>{
     /*const[showState, setShowState]= useState(false);*/
+
     const otherClick = () =>{
         if (title === "Automatizacion Industrial"){
             variableH = "Automatizacion Industrial";
@@ -29,10 +32,10 @@ const Item = ({nombreProducto, precioProducto, imagenProducto, stockProducto, id
                     <p>{nombreProducto}</p>
                     <span>u$s {precioProducto}</span>
                     <Link to ={`/product/${idProducto}`} className="link-color"><Button variant="contained" className="card__btn" onClick={otherClick}>Detalle</Button></Link>
-                    {<ItemCount stock={stockProducto} initial={1} onAdd={nombreProducto} setShowState={() => false}/>}
-
-                    {/*showState===false ?
-                        <ItemCount stock={stockProducto} initial={1} onAdd={nombreProducto} setShowState={setShowState}/> 
+                    
+                    
+                    {/* showState===false ?
+                        <ItemCount stock={stockProducto} initial={1} onAdd={nombreProducto} setShowState={setShowState} itemProducto={data}/> 
                         : 
                         <Button variant="contained" className="endCompra"><Link to ="/cart">Finalizar Compra</Link></Button>  
                     */} 
