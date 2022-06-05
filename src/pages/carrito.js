9e9ae28c2048f82b7f4adcbe7ccc05371a668a25
Carlos Mariano Belmonte <cbelmonte.laboral@gmail.com>
2022-06-05 12:10:54 -0300

    Se realiza el render del carrito

--- src/pages/carrito.js
+++ src/pages/carrito.js
@@ -1,8 +1,64 @@
 import {Grid} from '@mui/material';
+import nuevo from '../utils/nuevoMock'
+import '../pages/styles/carrito.css'
 
 const Carrito = () => {
     return(
-        <h1>Carrito por page</h1>
+        <div className='cart-container'>
+            <Grid className="grid-container" container spacing={0}>
+                <Grid item md={3}>
+                    <div className="div-img-item">
+                        <img className='img-item' alt="Imagen Item" src={`../${nuevo[4].imagen}`} />    
+                    </div>    
+                </Grid>
+                <Grid item md={6}>
+                    <h2 className="h2-item">{nuevo[4].nombre}</h2>
+                    <div className="p-group">
+                        <div className="p-item">Precio (Unidad): <p className="p-variable">u$s {`${nuevo[4].precio}`}</p></div>
+                        <div className="p-item">Cantidad seleccionada: <p className="p-variable">xxxx</p></div> 
+                        <div className="p-item">Stock disponible: <p className="p-variable">{`${nuevo[4].stock}`}</p></div>   
+                    </div>                        
+                </Grid>
+                <Grid item md={3}>
+                    <div>
+                        <ul className="list-group">
+                            <li className="list-group-item">Sub Total: u$s</li>
+                            <li className="list-group-item">IVA: u$s</li>
+                            <li className="list-group-item">TOTAL: u$s</li>
+                            <li className="list-group-item"><button>Eliminar Producto</button></li>
+                        </ul>     
+                    </div>
+                       
+                </Grid>
+            </Grid>
+            <Grid className="grid-container" container spacing={0}>
+                <Grid item md={3}>
+                    <div className="div-img-item">
+                        <img className='img-item' alt="Imagen Item" src={`../${nuevo[4].imagen}`} />    
+                    </div>    
+                </Grid>
+                <Grid item md={6}>
+                    <h2 className="h2-item">{nuevo[4].nombre}</h2>
+                    <div className="p-group">
+                        <div className="p-item">Precio (Unidad): <p className="p-variable">u$s {`${nuevo[4].precio}`}</p></div>
+                        <div className="p-item">Cantidad seleccionada: <p className="p-variable">xxxx</p></div> 
+                        <div className="p-item">Stock disponible: <p className="p-variable">{`${nuevo[4].stock}`}</p></div>   
+                    </div>                        
+                </Grid>
+                <Grid item md={3}>
+                    <div>
+                        <ul className="list-group">
+                            <li className="list-group-item">Sub Total: u$s</li>
+                            <li className="list-group-item">IVA: u$s</li>
+                            <li className="list-group-item">TOTAL: u$s</li>
+                            <li className="list-group-item"><button>Eliminar Producto</button></li>
+                        </ul>     
+                    </div>
+                       
+                </Grid>
+            </Grid>
+        </div>
+        
     )
 }
 
