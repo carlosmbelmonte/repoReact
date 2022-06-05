b48b4e7679e8abd5e669527dc7f47cd576edf2f1
Carlos Mariano Belmonte <cbelmonte.laboral@gmail.com>
2022-06-05 13:26:53 -0300

    Se agrega el cart por Contexto a la page del carrito

--- src/pages/carrito.js
+++ src/pages/carrito.js
@@ -1,67 +1,47 @@
 import {Grid} from '@mui/material';
-import nuevo from '../utils/nuevoMock'
 import '../pages/styles/carrito.css'
 import Button from '@mui/material/Button';
+import { useContext } from 'react';
+import CartContext from '../context/CartContext';
 
 const Carrito = () => {
+    const{ cart } = useContext(CartContext);
     return(
         <div className='cart-container'>
-            <Grid className="grid-container" container spacing={0}>
-                <Grid item md={3}>
-                    <div className="div-img-item">
-                        <img className='img-item' alt="Imagen Item" src={`../${nuevo[4].imagen}`} />    
-                    </div>    
-                </Grid>
-                <Grid item md={6}>
-                    <h2 className="h2-item">{nuevo[4].nombre}</h2>
-                    <div className="p-group">
-                        <div className="p-item">Precio (Unidad): <p className="p-variable">u$s {`${nuevo[4].precio}`}</p></div>
-                        <div className="p-item">Cantidad seleccionada: <p className="p-variable">xxxx</p></div> 
-                        <div className="p-item">Stock disponible: <p className="p-variable">{`${nuevo[4].stock}`}</p></div>   
-                    </div>                        
-                </Grid>
-                <Grid item md={3}>
-                    <div>
-                        <ul className="list-group">
-                            <li className="list-group-item">Sub Total: u$s</li>
-                            <li className="list-group-item">IVA: u$s</li>
-                            <li className="list-group-item">TOTAL: u$s</li>
-                            <li className="list-group-item"><div className="div-btn-ItemDelete"><Button variant="contained" className="cart__btnItem">Eliminar Producto</Button></div></li>
-                        </ul>     
-                    </div>
-                       
-                </Grid>
-            </Grid>
-            <Grid className="grid-container" container spacing={0}>
-                <Grid item md={3}>
-                    <div className="div-img-item">
-                        <img className='img-item' alt="Imagen Item" src={`../${nuevo[4].imagen}`} />    
-                    </div>    
-                </Grid>
-                <Grid item md={6}>
-                    <h2 className="h2-item">{nuevo[4].nombre}</h2>
-                    <div className="p-group">
-                        <div className="p-item">Precio (Unidad): <p className="p-variable">u$s {`${nuevo[4].precio}`}</p></div>
-                        <div className="p-item">Cantidad seleccionada: <p className="p-variable">xxxx</p></div> 
-                        <div className="p-item">Stock disponible: <p className="p-variable">{`${nuevo[4].stock}`}</p></div>   
-                    </div>                        
-                </Grid>
-                <Grid item md={3}>
-                    <div>
-                        <ul className="list-group">
-                            <li className="list-group-item">Sub Total: u$s</li>
-                            <li className="list-group-item">IVA: u$s</li>
-                            <li className="list-group-item">TOTAL: u$s</li>
-                            <li className="list-group-item"><div className="div-btn-ItemDelete"><Button variant="contained" className="cart__btnItem">Eliminar Producto</Button></div></li>
-                        </ul>     
-                    </div>
-                       
-                </Grid>
-            </Grid>
+            {cart.map( (elemento)=>{
+                return(
+                    <>
+                        <Grid className="grid-container" container spacing={0}>
+                            <Grid item md={3} >
+                                <div className="div-img-item">
+                                    <img className='img-item' alt="Imagen Item" src={`../${elemento.imagen}`} />    
+                                </div>    
+                            </Grid>
+                            <Grid item md={6} key={elemento.id}>
+                                <h2 className="h2-item">{elemento.nombre}</h2>
+                                <div className="p-group">
+                                    <div className="p-item">Precio (Unidad): <p className="p-variable">u$s {elemento.precio}</p></div>
+                                    <div className="p-item">Cantidad seleccionada: <p className="p-variable">{elemento.quantity}</p></div> 
+                                    <div className="p-item">Stock disponible: <p className="p-variable">{elemento.stock}</p></div>   
+                                </div>                        
+                            </Grid>
+                            <Grid item md={3} >
+                                <div>
+                                    <ul className="list-group">
+                                        <li className="list-group-item">Sub Total: u$s {(elemento.precio)*(elemento.quantity)}</li>
+                                        <li className="list-group-item">IVA: u$s {(elemento.precio)*(elemento.quantity)*(0.21)}</li>
+                                        <li className="list-group-item">TOTAL: u$s {(elemento.precio)*(elemento.quantity)*(1.21)}</li>
+                                        <li className="list-group-item"><div className="div-btn-ItemDelete"><Button variant="contained" className="cart__btnItem">Eliminar Producto</Button></div></li>
+                                    </ul>     
+                                </div>                               
+                           </Grid>
+                        </Grid>
+                    </>
+                )
+            })}
             <div className="cart-delete-btn">
                 <Button variant="contained" className='cart__btn'>Vaciar carrito</Button>    
-            </div>
-            
+            </div>            
         </div>
         
     )
