fa0faa1fd71dc071a060a2788b51443e993cd7eb
Carlos Mariano Belmonte <cbelmonte.laboral@gmail.com>
2022-06-09 17:04:24 -0300

    Se agrega un array para almacenar las cantidades

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -7,15 +7,22 @@ const CartProvider = ({children}) =>{
     let preciosItem = []; 
     let precioTotal = 0;
 
+    let quantityItem = [];
+    let quantityTotal = 0;                                                                                                                                                                                                            
+
     const addItem = (product) =>{
         let isInCart = cart.findIndex( cartItem => cartItem.nombre === product.nombre );
 
         if (isInCart === -1){
-            cart.push(product);
+            cart.push(product);           
             console.log("Se agrego el producto al carrito: ",cart);
             preciosItem.push( (product.precio)*(product.quantity)*(1.21) )  
+            quantityItem.push(product.quantity);
+
             console.log("el precio del producto es: ",preciosItem);
-            pTotal();
+            console.log("la cantidad del producto: ",quantityItem);
+
+            Total("precios");
             console.log("El total del carrito es: ", precioTotal);
             return cart  
         }else{
@@ -46,10 +53,18 @@ const CartProvider = ({children}) =>{
         console.log("El total del carrito es: ", precioTotal);
     }
 
-    const pTotal=()=>{
-        precioTotal = preciosItem.reduce((a, b) => a + b, 0);
+    const Total=(variable)=>{
+        if(variable==="precios"){
+            precioTotal = preciosItem.reduce((a, b) => a + b, 0);    
+        }
+        if(variable==="cantidades"){
+            quantityTotal = quantityItem.reduce((a, b) => a + b, 0);    
+        }
+        
         //return precioTotal;
     }
+
+    
     
     const sumatoria=()=>{       
         return precioTotal;
