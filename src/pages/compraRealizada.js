9ae7e939885c3a614d8b643f22cec11c5cbbcc2a
Carlos Mariano Belmonte <cbelmonte.laboral@gmail.com>
2022-06-15 16:59:40 -0300

    Se muestra en la page el detalle final de la compra

--- src/pages/compraRealizada.js
+++ src/pages/compraRealizada.js
@@ -8,14 +8,42 @@ const CompradoPage = () => {
     useEffect(() => {
         GetOrder(orderId)
                    .then((ord)=>{
-                        setOrden(ord)
+                        setOrden({...ord, id: orderId})
                    })  
     }, [orderId]); // eslint-disable-line
+
+    if (!orden.id) {
+        return <p>Cargando...</p>;
+    }
 
     return(
         <>
             <h1>Gracias por su compra</h1>
             {console.log(orden)}
+            <p>{orden.buyer.name}, gracias por su compra</p>
+            
+            <table>
+                <tbody>
+                <tr>
+                    <th>Producto</th>
+                    <th>Cantidad</th>
+                    <th>Subtotal</th>
+                    <th>Total</th>
+                </tr>
+                {orden.items.map((product) => {
+                    return (
+                    <tr key={product.id}>
+                        <td>{product.name}</td>
+                        <td>{product.quantity}</td>
+                        <td>u$s {product.price}</td>
+                        <td>u$s {(product.price)*(product.quantity)*(1.21)}</td>
+                    </tr>
+                    );
+                })}
+                </tbody>
+                
+            </table>
+            <p>El precio total FINAL es: u$s {orden.total}</p>
         </>
         
     )
