5a3ac18636c273f271469908115029f8d3fa7532
Carlos Mariano Belmonte <cbelmonte.laboral@gmail.com>
2022-06-22 10:21:58 -0300

    Se actualiza el css del home

--- src/pages/home.js
+++ src/pages/home.js
@@ -6,17 +6,13 @@ import { Link } from 'react-router-dom';
 
 const printFondo = () => {
     return(
-        <>
-            <main>
-                <section>
-                    <article>
-                        <h2>Somos lideres en instalaciones y equipamiento industrial</h2>
-                        <div className="home_container">
-                            <Button variant="contained" className="home__btn"><Link to ="/products" className="link-end">Conocé más</Link></Button>                              
-                        </div>
-                            </article>   
-                </section>
-            </main>           
+        <>           
+            <div className="menuindex">
+                <h2>Somos lideres en instalaciones y equipamiento industrial</h2>
+                <div className="home_container">
+                    <Button variant="contained" className="home__btn"><Link to ="/products" className="link-end">Conocé más</Link></Button>                              
+                </div>
+            </div>              
         </>
     )    
 }
