5d5f2ae6830dbf7aea6787fbaaabe3332d3be1b8
Carlos Mariano Belmonte <cbelmonte.laboral@gmail.com>
2022-06-23 14:12:11 -0300

    Se agrega una validacion para el envio de mensajes y se cambia lo mostrado en el formulario cuando se envia un mensaje

--- src/pages/contacts.js
+++ src/pages/contacts.js
@@ -2,10 +2,98 @@ import { Grid } from '@mui/material';
 import './styles/contacts.css';
 import { Container } from '@mui/material';
 import Box from '@mui/material/Box';
+import { useState } from 'react';
+import Button from '@mui/material/Button';
 
 const Contacto = () => {
+    const [validation, setValidation] = useState("");
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [msg, setMessage] = useState("");
+    const [flag, setFlag] = useState(true);
+
     const iframeSource = '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d13147.523808575534!2d-58.51419299999999!3d-34.53124400000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xd15e0d94908f499e!2sELECTRO%20SELEC!5e0!3m2!1ses!2sar!4v1655990588601!5m2!1ses!2sar" width="480" height="320" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>'
     
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    
+        setValidation("");
+        if (!name || !email || !msg) {
+            setValidation("(*)Son campos OBLIGATORIOS para realizar la consulta.");
+            return false;
+        }
+        const newMessage = {
+            person:  name,
+            mail: email,
+            date: new Date(),
+            message: msg
+        }
+        
+        console.log(newMessage);
+        setFlag(false)
+        /*saveMsg(newMessage)
+            .then((respuesta)=>{
+                console.log("1 - Respuesta mostrada en cart: ",respuesta);
+                //clear();
+            });*/        
+    }
+    
+    const sendNewMessage = () => {
+        setName("");
+        setEmail("");
+        setMessage("");
+        setFlag(true);
+    }
+
+    const flagMessage = () => {
+        if(flag===true){
+            return(
+                <>
+                    <form onSubmit={handleSubmit}>
+                        <fieldset>
+                            <legend>Cómo podemos ayudarte?</legend>
+                            <Grid container spacing={0}>
+                                <Grid item md={6}>
+                                    <div>
+                                        <label className="contact__label">Nombre y Apellido(*)</label>
+                                        <input className="contact__input" type="text" id="name" name="name" value={name} onChange={(e) => setName(e.target.value)}/>    
+                                    </div>
+                                    <div>
+                                        <label className="contact__label">Mail(*)</label>
+                                        <input className="contact__input" type="email" id="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)}/>    
+                                    </div>
+                                    {(validation === ''? '': <p>{validation}</p>)}
+                                </Grid>
+                                <Grid item md={5}>
+                                    <div>
+                                        <label className="contact__label">Mensaje(*)</label>
+                                        <textarea id="msg" name="msg" value={msg} placeholder="Escriba aqui su consulta" onChange={(e) => setMessage(e.target.value)}></textarea>
+                                        <div className="contact__divbtn">
+                                            <input type="submit" className="contact__submit" value="ENVIAR"/>
+                                        </div>
+                                    </div>
+                                </Grid>
+                            </Grid>   
+                        </fieldset>
+                        
+                    </form>
+                </>
+            )
+        }else{
+            return(
+                <>
+                    <form onSubmit={handleSubmit}>
+                        <fieldset>
+                            <legend>{name} su mensaje se envio exitosamente.</legend>
+                            <p>¿Desea enviar un nuevo mensaje?</p>
+                            <Button variant="contained" onClick={sendNewMessage}>SI</Button>
+                        </fieldset>
+                    </form>
+                </>
+            )
+
+        }
+    }
       
     return (
         <>
@@ -20,7 +108,7 @@ const Contacto = () => {
                                 <li>Estados Unidos 1200, Munro, Buenos Aires, Argentina</li>
                                 <li>(+54 11)8888-7474</li>
                                 <li>contacto@engenium.com.ar</li>
-                                <li>Horario de atencion:Lunes a Viernes de 8:00 a 17:00 hs</li>
+                                <li>Horario de atencion: Lunes a Viernes de 8:00 a 17:00 hs</li>
                             </ul>    
                         </div>                            
                     </Grid>
@@ -33,31 +121,7 @@ const Contacto = () => {
                         mb: 2
                     }}
                 >
-                    <form>
-                        <fieldset>
-                            <legend>Cómo podemos ayudarte?</legend>
-                            <Grid container spacing={0}>
-                                <Grid item md={6}>
-                                    <div>
-                                        <label className="contact__label">Nombre y Apellido(*)</label>
-                                        <input className="contact__input" type="text" id="nompre_apellidoInput1"/>    
-                                    </div>
-                                    <div>
-                                        <label className="contact__label">Mail(*)</label>
-                                        <input className="contact__input" type="email" id="mailInput1"/>    
-                                    </div>
-                                </Grid>
-                                <Grid item md={5}>
-                                    <div>
-                                        <label className="contact__label">Mensaje:</label>
-                                        <textarea id="mensajeTextarea1" placeholder="Escriba aqui su consulta"></textarea>
-                                        <input type="submit" value="ENVIAR"/>
-                                    </div>
-                                </Grid>
-                            </Grid>   
-                        </fieldset>
-                        <p>(*)Son campos OBLIGATORIOS para realizar la consulta.</p>
-                    </form>                    
+                    {flagMessage()}                   
                 </Box>
             </Container>
                     
