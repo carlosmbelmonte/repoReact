38f756fc3e6150c7994df787b626a68b57f73904
Carlos Mariano Belmonte <cbelmonte.laboral@gmail.com>
2022-06-23 16:08:19 -0300

    Se actualiza la visualizacion del home, lo que permite dejar el navbar por delante

--- src/pages/home.js
+++ src/pages/home.js
@@ -5,13 +5,15 @@ import { Link } from 'react-router-dom';
 const Home = () => {
     return(
         <>
-            <div className="menuindex">
-                <h2>Somos lideres en instalaciones y equipamiento industrial</h2>
-                <div className="home_container">
-                    <Button variant="contained" className="home__btn"><Link to ="/aboutus" className="link-end">Conocé más</Link></Button>                              
+            <div className="container_div">
+                <div className="div__position">
+                    <h2>Somos lideres en instalaciones y equipamiento industrial</h2>
+                    <div className="home_divbtn">
+                        <Button variant="contained" className="home__btn"><Link to ="/aboutus" className="link-end">Conocé más</Link></Button>                              
+                    </div>    
                 </div>
-            </div> 
-            
+                
+            </div>            
         </>
         
     )
