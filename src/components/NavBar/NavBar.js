348e5de968392b55e76f8a0f5fb20329d706c208
Carlos Mariano Belmonte <cbelmonte.laboral@gmail.com>
2022-06-27 12:45:30 -0300

    Se agrega un nuevo navBar en funcion mobile

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -7,15 +7,65 @@ import Button from '@mui/material/Button';
 import Avatar from '@mui/material/Avatar';
 import { Link } from 'react-router-dom';
 import CartWidget from '../CartWidget/CartWidget';
+import MenuIcon from '@mui/icons-material/Menu';
+import { useState } from 'react';
 
 let variableT;
 const NavBar = () => {
+    const [mobileOpen, setMobileOpen] = useState(false);
+
+    const handleDrawerToggle = () => {
+        setMobileOpen(!mobileOpen);
+    }
+
+    const showMenu=()=>{
+        if(mobileOpen){
+            return(
+                <>
+                    <ul>
+                        <li>
+                            <Button color="inherit" className="navbar__btn"><Link to ="/inicio" className="link-color">Inicio</Link></Button>
+                        </li>
+                        <li>
+                            <Button color="inherit" className="navbar__btn"><Link to ="/aboutus" className="link-color">Quienes somos</Link></Button>
+                        </li>
+                        <li>
+                            <Button color="inherit" className="navbar__btn"><Link to ="/projects" className="link-color">Proyectos</Link></Button>
+                        </li>
+                        <li>
+                            <Button color="inherit" className="navbar__btn"><Link to ="/products" className="link-color">Productos</Link></Button>
+                            <ul className="interior">
+                                <li>
+                                    <Button>
+                                        <Link to ="/products/automatizacion" className="link-color">
+                                            Automatizacion Industrial
+                                        </Link>
+                                    </Button>
+                                </li>
+                                <li>
+                                    <Button>
+                                        <Link to ="/products/medidores" className="link-color">
+                                            Medidores Trifasicos
+                                        </Link>
+                                    </Button>
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                            <Button color="inherit" className="navbar__btn"><Link to ="/contacts" className="link-color">Contacto</Link></Button>
+                        </li>
+                    </ul> 
+                </>
+            )
+        }
+    }
+
     return (
         <>
         <AppBar position="static">
             <Toolbar>
             <Grid container spacing={0}>
-                <Grid item md={3}>
+                <Grid item md={3} xs={12}>
                     <div className="container-logo">
                         <Link to ="/products" className="link-color">
                             <Avatar
@@ -24,7 +74,7 @@ const NavBar = () => {
                                 src="../logo.png"
                                 sx={{ width: 60, height: 60 }}
                             />
-                           <h1>ENGENIUM<span>|</span></h1>      
+                           <h1>ENGENIUM</h1>      
                         </Link>                               
                     </div>    
                 </Grid>
@@ -63,6 +113,25 @@ const NavBar = () => {
                                 <Button color="inherit" className="navbar__btn"><Link to ="/contacts" className="link-color">Contacto</Link></Button>
                             </li>
                         </ul>                
+                    </div>
+                    <div className="mobile-list">
+                        <Grid container spacing={0}>
+                            <Grid item xs={6}>
+                                <Button color="inherit" onClick={handleDrawerToggle} >
+                                    <MenuIcon />    
+                                </Button>      
+                            </Grid>
+                            <Grid item xs={6}>
+                                
+                            </Grid>
+                            <Grid item xs={12}>
+                                {showMenu()}
+                            </Grid>
+
+
+                        </Grid>
+                           
+                        
                     </div>
                 </Grid>
                 <Grid 
