ce1ebbea820c9e4f743bb7ff16c4b2e0fe840771
Carlos Mariano Belmonte <cbelmonte.laboral@gmail.com>
2022-06-27 11:16:49 -0300

    Se hace responsive la pagina AboutUs

--- src/components/YoutubeComponent/YoutubeEmbed.js
+++ src/components/YoutubeComponent/YoutubeEmbed.js
@@ -4,8 +4,7 @@ import PropTypes from "prop-types";
 const YoutubeEmbed = ({ embedId }) => (
   <div className="video-responsive">
     <iframe
-      width="560"
-      height="315"
+
       src={`https://www.youtube.com/embed/${embedId}`}
       frameBorder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
