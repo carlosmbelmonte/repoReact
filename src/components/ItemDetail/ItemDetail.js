60d8b581934e16155e089805a914f03f8ff83bac
Carlos Mariano Belmonte <cbelmonte.laboral@gmail.com>
2022-06-27 09:53:08 -0300

    Se hace responsive la pagina de detalles

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -34,18 +34,18 @@ const ItemDetail = ({data}) => {
         <>
             <div className="margin-boxies">
             <Grid className="border-grid" container spacing={0}>
-                <Grid className="border-gridSpacing" item md={4}>
+                <Grid className="border-gridSpacing" item md={4} xs={12}>
                     <img className="img-Detail" alt="FOTO PRODUCTO" src={`../${data.imagen}`} />
                 </Grid>
 
-                <Grid className="border-gridSpacing" item md={6}>
+                <Grid className="border-gridSpacing" item md={6} xs={12}>
                     <h2 className="h2-Detail">{data.nombre}</h2>
                     <div className="div-Detail">
                         <p>{data.descripcion}</p>
                         <p>Id: {data.id}</p>     
                     </div>
                     <Grid container spacing={0}>
-                        <Grid  item md={6}>
+                        <Grid  item md={6} xs={12}>
                             <div className="grid-interno">
                                 {showState===false ?
                                     <ItemCount stock={data.stock} initial={1} onAdd={agregarCantidadAlCarrito} setShowState={setShowState} itemProducto={data}/> 
@@ -57,13 +57,13 @@ const ItemDetail = ({data}) => {
                                 }     
                             </div>      
                         </Grid>  
-                        <Grid item md={6}>
+                        <Grid item md={6} xs={12}>
                             {irCarrito()}
                         </Grid>   
                     </Grid>                                   
                 </Grid>
 
-                <Grid item md={2}>
+                <Grid item md={2} xs={12}>
                     <h2 className="h2-Detail">Precio</h2>
                     <div className="div-Detail">
                         <p>u$s {data.precio}</p>    
